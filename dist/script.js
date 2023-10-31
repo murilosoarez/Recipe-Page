b2f8042cf07869626e215937e077e4ef38a5a0eb
murilosoarez <murilosoliveira18@gmail.com>
2023-10-31 13:20:57 -0300

    [Fix bottom layer of page]

--- dist/script.js
+++ dist/script.js
@@ -3,9 +3,11 @@ recipeCards = Array.from(recipeCards)
 
 recipeCards.forEach(function(recipeCard) {
     recipeCard.addEventListener('mouseenter', () => {
+        recipeCard.style.transition = 'transform 0.3s ease'
         recipeCard.style.transform = 'scale(1.15 , 1.15)'
     })
     recipeCard.addEventListener('mouseleave', () => {
+        recipeCard.style.transition = 'transform 0.3s ease'
         recipeCard.style.transform = 'scale(1 , 1)'
     })
 })
