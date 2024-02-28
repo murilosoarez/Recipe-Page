6ea51816f38634ed0dc6e1b6868f42d9e2401353
murilosoarez <murilosoliveira18@gmail.com>
2024-02-28 11:32:47 -0400

    [Update design]

--- script.js
+++ script.js
@@ -18,4 +18,29 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    let pageContent = document.getElementsByClassName('Main Content Display')
+    pageContent[0].style.opacity = "0"
+
+    document.addEventListener('scroll', (e) => {
+        console.log(window.scrollY)
+        if (window.scrollY == 3) {
+            pageContent[0].style.opacity = '0.2'
+        }
+        if (window.scrollY == 7) {
+            pageContent[0].style.opacity = '0.3'
+        }
+        if (window.scrollY = 21) {
+            pageContent[0].style.opacity = '0.4'
+        }
+        if (window.scrollY = 35) {
+            pageContent[0].style.opacity = '0.5'
+        }
+        if (window.scrollY = 49) {
+            pageContent[0].style.opacity = '0.6'
+        }
+        if (window.scrollY = 56) {
+            pageContent[0].style.opacity = '0.7'
+        }
+    })
+
 })
